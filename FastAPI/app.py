f6fa832c6d3457b7d9b902d398d6f6894a920bac
Prince Singh <princesingh15122004@gmail.com>
2026-01-14 11:23:50 +0530

    Created a model.pkl and backend in app.py to serve into frontend.py

---
+++ FastAPI/app.py
@@ -0,0 +1,83 @@
+from fastapi import FastAPI
+from pydantic import BaseModel,Field,computed_field
+from fastapi.responses import JSONResponse
+from typing import Literal,Annotated
+import pickle
+import pandas as pd
+
+# import the ml model
+with open('model.pkl','rb') as f:
+    model = pickle.load(f)
+
+app = FastAPI()
+
+tier_1_cities = ["Mumbai", "Delhi", "Bangalore", "Chennai", "Kolkata", "Hyderabad", "Pune"]
+tier_2_cities = [
+            "Jaipur", "Chandigarh", "Indore", "Lucknow", "Patna", "Ranchi", "Visakhapatnam", "Coimbatore",
+            "Bhopal", "Nagpur", "Vadodara", "Surat", "Rajkot", "Jodhpur", "Raipur", "Amritsar", "Varanasi",
+            "Agra", "Dehradun", "Mysore", "Jabalpur", "Guwahati", "Thiruvananthapuram", "Ludhiana", "Nashik",
+            "Allahabad", "Udaipur", "Aurangabad", "Hubli", "Belgaum", "Salem", "Vijayawada", "Tiruchirappalli",
+            "Bhavnagar", "Gwalior", "Dhanbad", "Bareilly", "Aligarh", "Gaya", "Kozhikode", "Warangal",
+            "Kolhapur", "Bilaspur", "Jalandhar", "Noida", "Guntur", "Asansol", "Siliguri"
+        ]
+
+# Pydantic model to validate incoming data
+class UserInput(BaseModel):
+    age: Annotated[int,Field(...,gt=0,lt=120,description='Age of the user')]
+    weight: Annotated[float,Field(...,gt=0,description='Weight of the user')]
+    height: Annotated[float,Field(...,gt=0,lt=2.5,description='Height of the user')]
+    income_lpa: Annotated[float,Field(...,gt=0,description='Annual Salary of the user in LPA')]
+    smoker: Annotated[bool,Field(...,description='Is user a smoker? ')]
+    city: Annotated[str,Field(...,description='city of the user')]
+    occupation: Annotated[Literal['retired', 'freelancer', 'student', 'government_job',
+       'business_owner', 'unemployed', 'private_job'],Field(...,description='Occupation of the patient')]
+    
+    @computed_field
+    @property
+    def bmi(self)->float:
+        return round(self.weight/(self.height**2),2)
+    
+    @computed_field
+    @property
+    def lifestyle_risk(self)-> str:
+        if self.smoker and self.bmi > 30:
+            return "high"
+        elif self.smoker and self.bmi > 27:
+            return "medium"
+        return "low"
+    
+    @computed_field
+    @property
+    def city_tier(self)->int:
+        if self.city in tier_1_cities:
+            return 1
+        elif self.city in tier_2_cities:
+            return 2
+        return 3
+    
+    @computed_field
+    @property
+    def age_group(self)->str:
+        if self.age < 25:
+            return "young"
+        elif self.age < 45:
+            return "adult"
+        elif self.age < 60:
+            return "midle_aged"
+        return "senior"
+    
+
+@app.post('/predict')
+def predict_premium(data:UserInput):
+    input_df = pd.DataFrame([{
+        'bmi': data.bmi,
+        'age_group': data.age_group,
+        'lifestyle_risk': data.lifestyle_risk,
+        'city_tier': data.city_tier,
+        'income_lpa': data.income_lpa,
+        'occupation':data.occupation
+    }])
+
+    prediction = model.predict(input_df)[0] #ek list milegi but uska bas hume 0th item chahiye hoga jo ki hamara predcition ka result hoga
+
+    return JSONResponse(status_code=200,content={'predicted_category':prediction})
